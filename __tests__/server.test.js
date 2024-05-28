21b61f3a2f60539f580e1c0a35e616ebf247535a
davidhcodes <dhanlyg1@gmail.com>
2024-05-28 10:42:45 +0100

    Creates the server.test.js file to enable testing of apis

---
+++ __tests__/server.test.js
@@ -0,0 +1,16 @@
+const format = require("pg-format")
+const db = require('../db/connection.js')
+const seed = require('../db/seeds/seed.js')
+const request = require('supertest')
+const express= require('express');
+const jestSorted = require('jest-sorted');
+
+
+
+describe("Testing APIs", () => {
+    test("Status  200 and return all topics", () => {
+     
+
+      })
+        
+    })
