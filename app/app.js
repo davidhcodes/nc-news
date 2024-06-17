1f3562498e5840c7577f2e639e0858efb1ab3c27
davidhcodes <dhanlyg1@gmail.com>
2024-06-17 10:18:55 +0100

    Uninstalls cors and removes references

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 // app.js
 
-const cors = require('cors')
+
 const express = require('express');
 
+ /* Importing the controller files */
+ const apiRouter = require('../routes/api-router');
 
 
 
-/* Middleware functions */
+ /* Middleware functions */
 const app = express();
-app.use(cors());
 app.use(express.json())
 
-/* Importing the controller files */
-const apiRouter = require('../routes/api-router');
 
  app.use('/api', apiRouter)
 
